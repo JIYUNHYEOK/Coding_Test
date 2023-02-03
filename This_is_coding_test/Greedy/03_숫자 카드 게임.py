eae6639598c8d51030abf7e14c7f72adfb64aac2
지윤혁 <yhji1127@gmail.com>
2023-02-03 13:04:02 +0900

    Delete This_is_coding_test/Greedy directory

--- This_is_coding_test/Greedy/03_숫자 카드 게임.py
+++
@@ -1,9 +0,0 @@
-n, m = map(int, input().split())
-
-result = 0
-for _ in range(n):
-    data = list(map(int, input().split()))
-    min_value = min(data)
-    result = max(result, min_value)
-   
-print(result)
